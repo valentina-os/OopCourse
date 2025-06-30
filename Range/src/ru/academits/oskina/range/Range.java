d80cff879a80b37c929bdbfac4cb46002c58a006
valentina-os <valentinaoskina92@mail.ru>
2025-06-30 22:43:22 +0700

    Add Range project

---
+++ Range/src/ru/academits/oskina/range/Range.java
@@ -0,0 +1,4 @@
+package ru.academits.oskina.range;
+
+public class Range {
+}
---
+++ Range/src/ru/academits/oskina/range_main/Main.java
@@ -0,0 +1,7 @@
+package ru.academits.oskina.range_main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
