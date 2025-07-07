06a8a9bb0033c287e19c4b3a0a6d10032cab56ff
valentina-os <valentinaoskina92@mail.ru>
2025-07-07 14:31:56 +0700

    Complete first part of Range project

--- Range/src/ru/academits/oskina/range/Range.java
+++ Range/src/ru/academits/oskina/range/Range.java
@@ -1,4 +1,35 @@
 package ru.academits.oskina.range;
 
 public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getRangeLength(Range range) {
+        return range.to - range.from;
+    }
+
+    public boolean isInside(double number) {
+        return number >= from && number <= to;
+    }
 }
--- Range/src/ru/academits/oskina/range_main/Main.java
+++ Range/src/ru/academits/oskina/range_main/Main.java
@@ -1,7 +1,32 @@
 package ru.academits.oskina.range_main;
 
+import ru.academits.oskina.range.Range;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world  ");
+        Scanner scanner = new Scanner(System.in);
+
+        Range range = new Range(1.1, 3.1);
+
+        System.out.println("Длина диапазона = " + range.getRangeLength(range));
+
+        range.setFrom(3);
+        System.out.println("Длина нового диапазона = " + range.getRangeLength(range));
+        System.out.println("Начало нового диапазона = " + range.getFrom());
+
+        range.setTo(6.6);
+        System.out.println("Длина нового диапазона = " + range.getRangeLength(range));
+        System.out.println("Конец нового диапазона = " + range.getTo());
+
+        System.out.print("Введите число: ");
+        double number = scanner.nextDouble();
+
+        if (range.isInside(number)) {
+            System.out.println("Указанное число входит в диапазон");
+        } else {
+            System.out.println("Указанное число не входит в диапазон");
+        }
     }
 }
