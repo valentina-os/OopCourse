1f2a09a86275bb233d024758d1d68edf1e1ba3ad
valentina-os <valentinaoskina92@mail.ru>
2025-07-17 14:34:31 +0700

    Complete second part of Range project

--- Range/src/ru/academits/oskina/range/Range.java
+++ Range/src/ru/academits/oskina/range/Range.java
@@ -25,11 +25,78 @@ public class Range {
         this.to = to;
     }
 
-    public double getRangeLength(Range range) {
-        return range.to - range.from;
+    public double getLength() {
+        return this.to - this.from;
     }
 
     public boolean isInside(double number) {
         return number >= from && number <= to;
+    }
+
+    public void printRange() {
+        System.out.println("от " + this.from + " до " + this.to);
+    }
+
+    public Range getIntersection(Range range) {
+        Range intersection = new Range(this.from, this.to);
+
+        if (range.from >= this.to || this.from >= range.to) {
+            intersection = null;
+        } else {
+            if (this.from < range.from) {
+                intersection.from = range.from;
+            }
+
+            if (this.to > range.to) {
+                intersection.to = range.to;
+            }
+        }
+
+        return intersection;
+    }
+
+    public Range[] getUnion(Range range) {
+        Range[] union = new Range[2];
+
+        if (range.from > this.to || this.from > range.to) {
+            union[0] = new Range(this.from, this.to);
+            union[1] = new Range(range.from, range.to);
+        } else {
+            union[0] = new Range(from, to);
+            union[1] = null;
+
+            if (this.from > range.from) {
+                union[0].from = range.from;
+            }
+
+            if (this.to < range.to) {
+                union[0].to = range.to;
+            }
+        }
+
+        return union;
+    }
+
+    public Range[] getSubtraction(Range range) {
+        Range[] subtraction = {null, null};
+
+        if (range.from >= this.to || this.from >= range.to) {
+            subtraction[0] = new Range(this.from, this.to);
+        } else {
+            if (this.from < range.from && this.to < range.to) {
+                subtraction[0] = new Range(this.from, range.from);
+            }
+
+            if (this.from > range.from && this.to > range.to) {
+                subtraction[0] = new Range(range.to, this.to);
+            }
+
+            if (this.from < range.from && this.to > range.to) {
+                subtraction[0] = new Range(this.from, range.from);
+                subtraction[1] = new Range(range.to, this.to);
+            }
+        }
+
+        return subtraction;
     }
 }
--- Range/src/ru/academits/oskina/range_main/Main.java
+++ Range/src/ru/academits/oskina/range_main/Main.java
@@ -8,16 +8,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Range range = new Range(1.1, 3.1);
+        Range range = new Range(1.5, 4.5);
 
-        System.out.println("Длина диапазона = " + range.getRangeLength(range));
+        System.out.println("Длина диапазона = " + range.getLength());
 
         range.setFrom(3);
-        System.out.println("Длина нового диапазона = " + range.getRangeLength(range));
+        System.out.println("Длина нового диапазона = " + range.getLength());
         System.out.println("Начало нового диапазона = " + range.getFrom());
 
         range.setTo(6.6);
-        System.out.println("Длина нового диапазона = " + range.getRangeLength(range));
+        System.out.println("Длина нового диапазона = " + range.getLength());
         System.out.println("Конец нового диапазона = " + range.getTo());
 
         System.out.print("Введите число: ");
@@ -27,6 +27,38 @@ public class Main {
             System.out.println("Указанное число входит в диапазон");
         } else {
             System.out.println("Указанное число не входит в диапазон");
+        }
+
+        Range range2 = new Range(2.5, 3.5);
+
+        if (range.getIntersection(range2) == null) {
+            System.out.println("Пересечения нет");
+        } else {
+            System.out.print("Интервал пересечения: ");
+            range.getIntersection(range2).printRange();
+        }
+
+        Range[] union = range.getUnion(range2);
+        System.out.println("Интервал(ы) объединения двух диапазонов: ");
+        union[0].printRange();
+
+        if (union[1] != null) {
+            union[1].printRange();
+        }
+
+        Range[] subtraction = range.getSubtraction(range2);
+        System.out.println("Интервал(ы) разности двух диапазонов: ");
+
+        if (subtraction[0] == null && subtraction[1] == null) {
+            System.out.println("Интервалов разности нет");
+        } else {
+            if (subtraction[0] != null) {
+                subtraction[0].printRange();
+            }
+
+            if (subtraction[1] != null) {
+                subtraction[1].printRange();
+            }
         }
     }
 }
